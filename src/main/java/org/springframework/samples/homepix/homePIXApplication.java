60ac6d69195c880454b67807bf96e316b614d453
elliottbignell <elliott.bignell@gmail.com>
2025-02-21 11:44:00 +0100

    Enabled cacheing for app

--- src/main/java/org/springframework/samples/homepix/homePIXApplication.java
+++ src/main/java/org/springframework/samples/homepix/homePIXApplication.java
@@ -19,6 +19,7 @@ package org.springframework.samples.homepix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * homePIX Spring Boot Application.
@@ -28,6 +29,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication(proxyBeanMethods = false)
 @EnableJpaRepositories(basePackages = "org.springframework.samples.homepix")
+@EnableCaching
 public class homePIXApplication {
 
 	// https://api.flickr.com/services/rest/?method=flickr.photosets.getList&api_key=4653c7a47e46e9c6f0ff19388000b524&user_id=50027087@N00&format=json&nojsoncallback=1
