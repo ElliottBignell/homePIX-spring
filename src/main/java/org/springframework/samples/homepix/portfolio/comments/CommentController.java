52c796204bf730e450a23a3e78b464f7eb3a02b2
elliottbignell <elliott.bignell@gmail.com>
2025-12-06 14:38:13 +0100

    Added Delete button and backend #8

--- src/main/java/org/springframework/samples/homepix/portfolio/comments/CommentController.java
+++ src/main/java/org/springframework/samples/homepix/portfolio/comments/CommentController.java
@@ -1,17 +1,23 @@
 package org.springframework.samples.homepix.portfolio.comments;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.samples.homepix.User;
 import org.springframework.samples.homepix.UserRepository;
 import org.springframework.samples.homepix.portfolio.collection.PictureFile;
 import org.springframework.samples.homepix.portfolio.collection.PictureFileRepository;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
 import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class CommentController {
@@ -26,6 +32,7 @@ public class CommentController {
 	UserRepository userRepository;
 
 	@PostMapping("/submit_comment")
+	@Secured("ROLE_USER")
 	public String submitComment(@RequestParam("send_comment") String text,
 								@RequestParam("description_id") Integer pictureId,
 								@RequestParam("redirectTo") String redirectTo,
@@ -56,6 +63,30 @@ public class CommentController {
 		comment.setUser(user);
 
 		commentRepository.save(comment);
+
+		return "redirect:" + redirectTo;
+	}
+
+	@PostMapping("/delete_comment")
+	@Secured("ROLE_USER")
+	public String deletePicture(
+		@RequestParam("redirectTo") String redirectTo,
+		@RequestParam("userId") String userId,
+		@RequestParam("commentId") String commentId,
+		Principal principal,
+		RedirectAttributes redirectAttributes
+	)
+	{
+		if (!redirectTo.startsWith("/")) {
+			redirectTo = "/"; // fallback to home
+		}
+
+		if (principal.getName().equals(userId)) {
+
+			Optional<Comment> comment = commentRepository.findById(Long.valueOf(commentId));
+
+			comment.ifPresent(value -> commentRepository.delete(value));
+		}
 
 		return "redirect:" + redirectTo;
 	}
